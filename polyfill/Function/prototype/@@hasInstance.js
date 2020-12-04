78b02fc0fbcb0a87a077f52a1e791fe76b7727ec
linsk1998 <h@linsk.us>
2020-12-04 19:19:35 +0800

    Function

---
+++ polyfill/Function/prototype/@@hasInstance.js
@@ -0,0 +1,7 @@
+import { Symbol } from "sky-core/pure/Symbol";
+
+if(!Function.prototype[Symbol.hasInstance]) {
+	Function.prototype[Symbol.hasInstance] = function(instance) {
+		return instance instanceof this;
+	};
+}
