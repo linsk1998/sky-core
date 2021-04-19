a78e73be9e9c02df3799d82f8c40343ddf61dbd7
linsk1998 <linsk@live.com>
2021-04-19 12:52:32 +0800

    AggregateError

--- impl/AggregateError.js
+++ impl/AggregateError.js
@@ -1,4 +1,4 @@
-import { inherits } from "sky-core/utils/inherits";
+import { inherits } from "sky-core";
 function AggregateError(errors, message) {
 	Error.call(this, message);
 	this.errors = errors;
