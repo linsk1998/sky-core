4576b50569b8d141f185a915454aaec61e77eefc
linsk1998 <h@linsk.us>
2023-04-25 20:19:31 +0800

    getOwnPropertyDescriptors

--- polyfill-modern/Object/getOwnPropertyDescriptors.js
+++ polyfill-modern/Object/getOwnPropertyDescriptors.js
@@ -1,7 +1,10 @@
 import { Object } from "../../native/Object";
-import { getOwnPropertyDescriptors } from "../../impl-modern/Object/getOwnPropertyDescriptors";
+import { defineProperty } from "../../native/Object/defineProperty";
+import { ff_getOwnPropertyDescriptors, ie_getOwnPropertyDescriptors } from "../../impl-modern/Object/getOwnPropertyDescriptors";
 if(!Object.getOwnPropertyDescriptors) {
-	if(Object.prototype.__defineSetter__) {
-		Object.getOwnPropertyDescriptors = getOwnPropertyDescriptors;
+	if(defineProperty) {
+		Object.getOwnPropertyDescriptors = ie_getOwnPropertyDescriptors;
+	} else if(Object.prototype.__defineSetter__) {
+		Object.getOwnPropertyDescriptors = ff_getOwnPropertyDescriptors;
 	}
 }
