7196440f1a7a1921ef384e39b8ce27d617ca68da
linsk1998 <h@linsk.us>
2024-04-15 00:13:50 +0800

    metadata

---
+++ pure/Symbol/metadata.js
@@ -0,0 +1,14 @@
+import { Symbol } from "../../native/Symbol";
+import { defineProperty } from "../../native/Object/defineProperty";
+import { nonEnumerable } from "../../support/nonEnumerable";
+
+export default (function() {
+	if(!Symbol) {
+		if(nonEnumerable) {
+			defineProperty(Object.prototype, '@@metadata', { enumerable: false, configurable: false, writable: true });
+		}
+		return '@@metadata';
+	} else {
+		return Symbol.metadata || Symbol('metadata');
+	}
+})();
