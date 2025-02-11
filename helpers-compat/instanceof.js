a3b3e3a76d6f53cdab5b823fcf01010146dc1908
linsk1998 <linsk@live.com>
2025-02-11 22:29:38 +0800

    helpers

--- helpers-compat/instanceof.js
+++ helpers-compat/instanceof.js
@@ -1,10 +1,10 @@
 import hasInstance from "../pure-compat/Symbol/hasInstance";
 
-export default function _instanceof(n, e) {
-	return null != e && e[hasInstance] ? !!e[Symbol.hasInstance](n) : __instanceof(n, e);
+export default function _instanceof(o, Class) {
+	return null != Class && Class[hasInstance] ? !!Class[Symbol.hasInstance](o) : __instanceof(o, Class);
 }
 
-function __instanceof(n, e) {
-	var p = n.__proto__;
-	return p ? __instanceof(p, e) : n === e.prototype || n instanceof e;
+function __instanceof(o, Class) {
+	var p = o.__proto__;
+	return p ? __instanceof(p, Class) : o === Class.prototype || o instanceof Class;
 }
